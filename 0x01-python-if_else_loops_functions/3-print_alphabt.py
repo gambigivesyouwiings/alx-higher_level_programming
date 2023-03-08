5b3f22ec4537b2265b2d5552383850d4a7156d2d
victor mugambi <gambikimathi@gmail.com>
2023-03-08 10:17:00 -0800

    updates

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for a in range(97,123):
-    if a in [101,113]:
+for a in range(97, 123):
+    if a in [101, 113]:
         pass
     else:
         print("{:c}".format(a), end="")
-
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for num in range(0,99):
+for num in range(0, 99):
     print("{:02d}, ".format(num), end="")
 print(f"{99}")
-
