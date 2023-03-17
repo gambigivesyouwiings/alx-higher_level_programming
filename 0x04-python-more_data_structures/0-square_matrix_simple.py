bb44cee9640e8d2e2de0c6167f8b2e4a812ae849
victor mugambi <gambikimathi@gmail.com>
2023-03-17 13:09:58 -0700

    data structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     square = []
-    for row in matrix:
+    new = matrix
+    for row in new:
         b = list(map(lambda x: x*x, row))
         square.append(b)
     return square
-
-matrix = [[3,2,1],[5,6,7]]
-print(square_matrix_simple(matrix))
