557865e5f80bf30d7f574175c3470247b92e442d
victor mugambi <gambikimathi@gmail.com>
2023-03-26 23:19:02 -0700

    Initial commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+def safe_print_list(my_list=[], x=0):
+    listed = []
+    if x==0:
+        listed.append(my_list[0])
+
+    for item in range(0, x):
+        try:
+            listed.append(my_list[item])
+        except IndexError:
+            break
+    return listed
