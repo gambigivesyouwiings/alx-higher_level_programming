877187cf4bc3524aa57896f5f1ce6742cc914b28
victor mugambi <gambikimathi@gmail.com>
2023-03-28 08:56:06 -0700

    correction

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,8 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
+        return True
     except ValueError as typ:
         sys.stderr.write(f"Exception: {typ}\n")
         return False
-    else:
-        return True
