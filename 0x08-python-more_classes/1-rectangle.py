b820b0bb6a8cbe031c9fe7b218b20b189ce075ae
victor mugambi <gambikimathi@gmail.com>
2023-04-06 22:32:16 -0700

    1-rect

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""A class that defines a rectangle"""
+
+
+class Rectangle:
+    """An empty class that represents a rectangle"""
+    def __init__(self, width=0, height=0):
+        if width < 0:
+            raise ValueError(message="width must be >= 0")
+        elif type(width) != int:
+            raise TypeError("width must be an integer")
+        else:
+            self.__width = width
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        elif type(height) != int:
+            raise TypeError(message="height must be an integer")
+        else:
+            self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) == int:
+            self.__width = value
+            return self.width
+    
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) == int:
+            self.__height = value
+            return self.__height
+
+
