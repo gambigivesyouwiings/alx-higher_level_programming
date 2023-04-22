bd1cbe73aec23966652813e5c0b372071e8e06a0
Victor <gambikimathi@gmail.com>
2023-04-22 04:44:29 -0700

    bonus

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,31 +1,39 @@
 #!/usr/bin/python3
 """
-    contains class Square which implements classes Rectangle & Base.
+    contains class Square implements class Rectangle
 """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """
+        Square implements rectangle
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        """ initializes the instance fromthe inherited class"""
+        """
+            initialises Square (overrides Rectangle init)
+        """
         super().__init__(size, size, x, y, id)
-
-    def __str__(self):
-        """ returns: a string representation for the object """
-        return(f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}")
 
     @property
     def size(self):
-        """ returns: private size attribute, by getter method """
-        return super().width
+        """
+            returns the size of the square
+        """
+        return self.width
 
     @size.setter
-    """ I am the size setter method """
     def size(self, value):
-        if not super().check_type("width", value):
-            Rectangle.width = value
-        if not super().check_type("height", value):
-            Rectangle.height = value
+        """
+            sets the value of size
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
         """
@@ -35,19 +43,31 @@ class Square(Rectangle):
                 *args -  variable number of no-keyword args
                 **kwargs - variable number of keyworded args
         """
-        super().update(*args, **kwargs)
+        if len(args) == 0:
+            for key, val in kwargs.items():
+                self.__setattr__(key, val)
+            return
 
         try:
-            self.height = self.size
+            self.id = args[0]
+            self.size = args[1]
             self.x = args[2]
             self.y = args[3]
-
         except IndexError:
             pass
 
-    def to_dictionary(self):
-        """ returns dictionary representation of instance """
+    def __str__(self):
+        """
+            Overloading str function
+        """
+        return "[{}] ({}) {}/{} - {}".format(type(self).__name__,
+                                             self.id, self.x, self.y,
+                                             self.width)
 
+    def to_dictionary(self):
+        """
+            Returns the dictionary representation of a Square
+        """
         return {'id': getattr(self, "id"),
                 'size': getattr(self, "width"),
                 'x': getattr(self, "x"),
