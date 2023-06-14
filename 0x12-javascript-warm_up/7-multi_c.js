00bdcbd71c7c46941166d2c5a157d0443d1b4de4
victor mugambi <gambikimathi@gmail.com>
2023-06-14 04:44:39 -0700

    7 tasks

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const myvar = Math.floor(process.argv[2])
-if (isNaN(myvar)) {
+const x = Math.floor(process.argv[2])
+if (isNaN(x)) {
 	console.log('Missing number of occurrences');}
-else {for (let i=0;i<myvar;i++){
+else {for (let i=0;i<x;i++){
 	console.log('C is fun')}
 }
 
